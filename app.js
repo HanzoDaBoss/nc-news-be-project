44dd534d658811fe3eab6af07e9e57be14a59e04
HanzoDaBoss <hanif.uddz@gmail.com>
2024-04-15 13:14:42 +0100

    complete task 3

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const {getTopics} = require("./controllers/topics-controllers");
+const {getEndpoints} = require("./controllers/endpoints-controllers");
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+
+app.get("/api", getEndpoints);
 
 app.get("*", (request, response, next) => {
   response.status(404).send({msg: "Not found"});
