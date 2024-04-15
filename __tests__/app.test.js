9eac865bfba69e917866743eeeab3fda51395497
HanzoDaBoss <hanif.uddz@gmail.com>
2024-04-15 14:30:37 +0100

    error handling, solution and testing for task 4 get /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -49,3 +49,50 @@ describe("/api", () => {
       });
   });
 });
+
+describe("/api/articles/:article_id", () => {
+  test("GET 200: responds with an article object corresponding to the passed id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({body}) => {
+        const {article} = body;
+        expect(article).toHaveProperty("author", "butter_bridge");
+        expect(article).toHaveProperty(
+          "title",
+          "Living in the shadow of a great man"
+        );
+        expect(article).toHaveProperty("article_id", 1);
+        expect(article).toHaveProperty(
+          "body",
+          "I find this existence challenging"
+        );
+        expect(article).toHaveProperty("topic", "mitch");
+        expect(article).toHaveProperty(
+          "created_at",
+          "2020-07-09T20:11:00.000Z"
+        );
+        expect(article).toHaveProperty("votes", 100);
+        expect(article).toHaveProperty(
+          "article_img_url",
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        );
+      });
+  });
+  test("GET 404: responds with a status and error message if article id is not found in database", () => {
+    return request(app)
+      .get("/api/articles/100")
+      .expect(404)
+      .then(({body}) => {
+        expect(body.msg).toBe("Article not found");
+      });
+  });
+  test("GET 400: responds with a status and error message if article id is invalid", () => {
+    return request(app)
+      .get("/api/articles/invalid_id")
+      .expect(400)
+      .then(({body}) => {
+        expect(body.msg).toBe("Bad request");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const {getTopics} = require("./controllers/topics-controllers");
 const {getEndpoints} = require("./controllers/endpoints-controllers");
+const {getArticleById} = require("./controllers/articles-controllers");
 
 const app = express();
-app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
 app.get("/api", getEndpoints);
 
+app.get("/api/articles/:article_id", getArticleById);
+
 app.get("*", (request, response, next) => {
   response.status(404).send({msg: "Not found"});
+});
+
+app.use((error, request, response, next) => {
+  if (error.status) {
+    response.status(error.status).send({msg: error.msg});
+  } else next(error);
+});
+
+app.use((error, request, response, next) => {
+  if (error.code === "22P02") {
+    response.status(400).send({msg: "Bad request"});
+  } else next(error);
 });
 
 module.exports = app;
