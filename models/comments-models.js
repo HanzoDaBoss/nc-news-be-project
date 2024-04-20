db1fc2cc2ff28be4da05cdf8525f94fae17ce14b
HanzoDaBoss <hanif.uddz@gmail.com>
2024-04-20 13:42:17 +0100

    Error handling, testing and solutions for Task 18 PATCH /api/comments/:comment_id

--- models/comments-models.js
+++ models/comments-models.js
@@ -63,8 +63,36 @@ function removeCommentById(comment_id) {
     });
 }
 
+function updateCommentById(comment_id, {inc_votes}) {
+  const updateVals = [inc_votes, comment_id];
+
+  if (updateVals.includes(undefined)) {
+    return Promise.reject({status: 400, msg: "Bad request"});
+  }
+
+  return db
+    .query(
+      `
+  UPDATE comments SET votes = votes + $1
+  WHERE comment_id = $2
+  RETURNING *;
+  `,
+      updateVals
+    )
+    .then(({rows}) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Comment not found",
+        });
+      }
+      return rows[0];
+    });
+}
+
 module.exports = {
   selectCommentsByArticleId,
   insertCommentByArticleId,
   removeCommentById,
+  updateCommentById,
 };
